b930af16c25da725708cb418d9c5f1fbe9a9a943
beaulieu527 <beaulieudev@gmail.com>
2019-05-01 20:32:50 -0400

    Done.

---
+++ db/migrate/20190502002819_add_ride_id_and_passenger_id_to_taxi.rb
@@ -0,0 +1,6 @@
+class AddRideIdAndPassengerIdToTaxi < ActiveRecord::Migration[5.0]
+  def change
+    add_column :taxis, :ride_id, :integer
+    add_column :taxis, :passenger_id, :integer
+  end
+end
